a8b55fc147e08280cd3c05e732ab81e0e2cd10bc
pfitzgerald <Patrick.Fitzgerald@agoda.com>
2015-07-29 15:22:22 +0700

    Updated column file and removal of sort dependency

--- src/scripts/column.js
+++ src/scripts/column.js
@@ -29,17 +29,6 @@ angular.module('ngm')
     'use strict';
 
     /**
-     * moves a widget in between a column
-     */
-    function moveWidgetInColumn($scope, column, evt){
-      var widgets = column.widgets;
-      // move widget and apply to scope
-      $scope.$apply(function(){
-        widgets.splice(evt.newIndex, 0, widgets.splice(evt.oldIndex, 1)[0]);
-      });
-    }
-
-    /**
      * finds a widget by its id in the column
      */
     function findWidget(column, index){
@@ -85,74 +74,6 @@ angular.module('ngm')
       return id ? parseInt(id) : -1;
     }
 
-    /**
-     * adds a widget to a column
-     */
-    function addWidgetToColumn($scope, model, targetColumn, evt){
-      // find source column
-      var cid = getId(evt.from);
-      var sourceColumn = findColumn(model, cid);
-
-      if (sourceColumn){
-        // find moved widget
-        var wid = getId(evt.item);
-        var widget = findWidget(sourceColumn, wid);
-
-        if (widget){
-          // add new item and apply to scope
-          $scope.$apply(function(){
-			if (!targetColumn.widgets) {
-				targetColumn.widgets = [];
-			}
-			
-            targetColumn.widgets.splice(evt.newIndex, 0, widget);
-          });
-        } else {
-          $log.warn('could not find widget with id ' + wid);
-        }
-      } else {
-        $log.warn('could not find column with id ' + cid);
-      }
-    }
-
-    /**
-     * removes a widget from a column
-     */
-    function removeWidgetFromColumn($scope, column, evt){
-      // remove old item and apply to scope
-      $scope.$apply(function(){
-        column.widgets.splice(evt.oldIndex, 1);
-      });
-    }
-
-    /**
-     * enable sortable
-     */
-    function applySortable($scope, $element, model, column){
-      // enable drag and drop
-      var el = $element[0];
-      var sortable = Sortable.create(el, {
-        group: 'widgets',
-        handle: '.ngm-move',
-        ghostClass: 'placeholder',
-        animation: 150,
-        onAdd: function(evt){
-          addWidgetToColumn($scope, model, column, evt);
-        },
-        onRemove: function(evt){
-          removeWidgetFromColumn($scope, column, evt);
-        },
-        onUpdate: function(evt){
-          moveWidgetInColumn($scope, column, evt);
-        }
-      });
-
-      // destroy sortable on column destroy event
-      $element.on('$destroy', function () {
-        sortable.destroy();
-      });
-    }
-
     return {
       restrict: 'E',
       replace: true,
@@ -169,16 +90,10 @@ angular.module('ngm')
         if (!col.cid){
           col.cid = dashboard.id();
         }
-
-        if (angular.isDefined(col.rows) && angular.isArray(col.rows)) {
-          // be sure to tell Angular about the injected directive and push the new row directive to the column
-          $compile(rowTemplate)($scope, function(cloned) {
-            $element.append(cloned);
-          });
-        } else {
-          // enable drag and drop for widget only columns
-          applySortable($scope, $element, $scope.ngmModel, col);
-        }
+        // be sure to tell Angular about the injected directive and push the new row directive to the column
+        $compile(rowTemplate)($scope, function(cloned) {
+          $element.append(cloned);
+        });
       }
     };
   });
